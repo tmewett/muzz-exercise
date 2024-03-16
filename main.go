3ca8832c1193a0ac718944316a6d24f6a171adb9
Tom M <tom@tmewett.com>
2024-03-16 13:17:53 +0000

    Add pgx, pgxpool, jwt imports

--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"log"
 	"math/rand/v2"
-
-	"github.com/labstack/echo/v4"
 	"net/http"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/labstack/echo/v4"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type User struct {
